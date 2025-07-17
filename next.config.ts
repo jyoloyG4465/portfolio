8ac0e8b5f66bde4275c9b737772d37b7c23ab803
jyoroyG <sriki1552@gmail.com>
2025-07-17 22:11:29 +0900

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export", // 静的出力を有効にする
+  trailingSlash: true, // 各ページのパス末尾に `/` をつける（index.htmlが必要なため）
 };
+module.exports = nextConfig;
 
 export default nextConfig;
