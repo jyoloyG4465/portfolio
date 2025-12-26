ed04c5321a1adf62556053304b9ab42c38462dc6
jyoloyG <sriki1552@gmail.com>
2025-12-26 22:49:07 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import "./globals.css";
 import "./globals.scss";
 import Header from "./component/Header/Header";
 import AboutMe from "./component/AboutMe/AboutMe";
